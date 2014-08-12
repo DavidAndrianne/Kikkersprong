804643be95990767cc567cd3abb3424c03f633d2
DavidAndrianneMobApp <David.andrianne@student.khleuven.be>
2014-08-12 16:38:48 +0200

    Uploaded the GIT folder instead of the Android 1
    Moved duplicates to the correct folders

--- Kikkersprong/src/mobile/apps/kikkersprong/MainActivity.java
+++ Kikkersprong/src/mobile/apps/kikkersprong/MainActivity.java
@@ -1,20 +1,39 @@
 package mobile.apps.kikkersprong;
 
+import mobile.apps.kikkersprong.db.Database;
+import mobile.apps.kikkersprong.db.DatabaseOnline;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
+	private Database db = new DatabaseOnline();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if(hasWifiConnection()){
+        	Log.v("databaseTest", "Connection to database = "+db.testConnection());
+        	db = (db.testConnection()) ? new DatabaseOnline() : new DatabaseOffine();
+        } else {
+        	db = new DatabaseOffline();
+        }
+    }
+    
+    private boolean hasWifiConnection(){
+    	ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return mWifi.isConnected();
     }
 
 
--- gen/mobile/apps/kikkersprong/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package mobile.apps.kikkersprong;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
